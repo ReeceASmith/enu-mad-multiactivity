655e8fc53d31cd441746b7cd27b323d718e5a57a
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-02-22 19:44:53 +0000

    Added multiple activities

--- app/src/main/java/napier/mad/multiactivity/ActivityA.java
+++ app/src/main/java/napier/mad/multiactivity/ActivityA.java
@@ -1,7 +1,11 @@
 package napier.mad.multiactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -13,5 +17,17 @@ public class ActivityA extends AppCompatActivity {
 		setContentView(R.layout.activitya);
 		
 		Toast.makeText(getBaseContext(), "In Activity A", Toast.LENGTH_SHORT).show();
+		
+		String stringReturn = "Edinburgh Napier University";
+		Intent returnIntent = new Intent();
+		returnIntent.putExtra("String", stringReturn);
+		setResult(1, returnIntent);
+		
+		Button btn_close = (Button)findViewById(R.id.btn_close);
+		btn_close.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 }
--- app/src/main/java/napier/mad/multiactivity/MainActivity.java
+++ app/src/main/java/napier/mad/multiactivity/MainActivity.java
@@ -1,11 +1,13 @@
 package napier.mad.multiactivity;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -14,13 +16,37 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button btn_newActivity = (Button)findViewById(R.id.btn_newActivity);
-		btn_newActivity.setOnClickListener(new View.OnClickListener() {
+		Button btn_activityA = (Button)findViewById(R.id.btn_activityA);
+		btn_activityA.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				//Intent activityA = new Intent(MainActivity.this, ActivityA.this);
 				Intent activityA = new Intent(MainActivity.this, ActivityA.class);
-				startActivity(activityA);
+				startActivityForResult(activityA, 1);
 			}
 		});
+		
+		
+		Button btn_activityB = (Button)findViewById(R.id.btn_activityB);
+		btn_activityB.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent activityB = new Intent(MainActivity.this, ActivityB.class);
+				startActivityForResult(activityB, 2);
+			}
+		});
+	}
+	
+	
+	
+	
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == 1) { // Respond to the right request ID
+			if (resultCode == 1) {
+				String returnString = data.getStringExtra("String");
+				Toast.makeText(getBaseContext(), "In main activity. String = " + returnString, Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 }
