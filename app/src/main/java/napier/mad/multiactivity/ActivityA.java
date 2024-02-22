479765a25aa136311aa405aef424571a53b40a0e
Reece Smith <40872343+ReeceASmith@users.noreply.github.com>
2024-02-22 20:05:05 +0000

    Completed Lab 03

--- app/src/main/java/napier/mad/multiactivity/ActivityA.java
+++ app/src/main/java/napier/mad/multiactivity/ActivityA.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -27,6 +29,28 @@ public class ActivityA extends AppCompatActivity {
 		btn_close.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				finish();
+			}
+		});
+		
+		
+		EditText txt_entry = (EditText)findViewById(R.id.txt_entry);
+		Button btn_updateReturnString = (Button)findViewById(R.id.btn_updateReturnString);
+		btn_updateReturnString.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				returnIntent.removeExtra("String");
+				returnIntent.putExtra("String", txt_entry.getText().toString());
+				//setResult(1, returnIntent);
+			}
+			
+		});
+		
+		
+		txt_entry.setOnKeyListener(new View.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				returnIntent.removeExtra("String");
+				returnIntent.putExtra("String", txt_entry.getText().toString());
+				return true;
 			}
 		});
 	}
